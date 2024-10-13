b3c72faddf9cd676a8c444b28e0aa7a01b50294c
Mohammad Imtiaj <73713309+Imtiaj19@users.noreply.github.com>
2024-10-13 17:33:16 +0000

    Type conversion is JS

---
+++ 02typeconversion.js
@@ -0,0 +1,19 @@
+let num = "33"
+let a = undefined
+let b = null
+let x = "abcd35"
+let y = Number(x)
+let Num = Number(num)
+console.log(typeof num)
+console.log(typeof Num)
+console.log(typeof x)
+
+console.log(typeof y)
+
+console.log(typeof b)
+console.log(y)
+console.log(b)
+
+
+
+
