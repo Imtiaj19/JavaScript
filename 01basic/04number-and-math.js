fcd33fe6116973f9468d732a8ad92c7779dabd5f
Mohammad Imtiaj <73713309+Imtiaj19@users.noreply.github.com>
2024-10-14 08:28:32 +0000

    math and Nums

---
+++ 01basic/04number-and-math.js
@@ -0,0 +1,21 @@
+let  num = new Number(105)
+// console.log(num);
+// console.log(num.toString().length);
+// console.log(typeof num);
+
+let num1 = new Number(105.94794)
+// console.log(num1.toFixed(3));  
+const othernum = 123.89993964923;
+// console.log(othernum.toPrecision(4));
+const Num = 10000000000;
+
+// console.log(Num.toLocaleString('en-IN'))
+
+console.log(Math.random() * 10 + 1);
+
+const mn = 10
+const mx = 100
+console.log(Math.floor(Math.random() * (mx - mn + 1)) + mn); // find random value in the range of mn to mx including
+
+
+
