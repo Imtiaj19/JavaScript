33f80807246830daa95e2ab0843d501a23f09738
Mohammad Imtiaj <73713309+Imtiaj19@users.noreply.github.com>
2024-10-14 08:29:51 +0000

    TypeConversion

---
+++ 01basic/02typeconversion.js
@@ -0,0 +1,22 @@
+let num = "33"
+let a = undefined
+let b = null
+let x = "abcd35"
+let y = Number(x)
+let bool = true
+let Num = Number(num)
+console.log(typeof num)
+console.log(typeof Num)
+console.log(typeof x)
+
+console.log(typeof y)
+
+console.log(typeof b)
+console.log(y)
+console.log(b)
+console.log(bool);
+// console.log("2" === 2);
+
+
+
+
