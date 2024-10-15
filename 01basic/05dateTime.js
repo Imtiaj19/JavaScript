23e04698cbde257c302cdea03e83b68e5ff17014
Mohammad Imtiaj <73713309+Imtiaj19@users.noreply.github.com>
2024-10-15 17:25:22 +0000

    DatTime ij JS

---
+++ 01basic/05dateTime.js
@@ -0,0 +1,13 @@
+let dateTime = new Date()
+// console.log(dateTime);
+// console.log(dateTime.toLocaleString())
+// console.log(dateTime.toString());
+// console.log(dateTime.getMonth());
+// console.log(dateTime.getYear());
+
+
+console.log(dateTime.getTime());  // print time in millisecond as same as date.now()
+console.log(Date.now())
+
+
+
